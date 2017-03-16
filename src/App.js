aa57dff882ae6de8c1b505bcaa36622725e7e0cd
Jon Gregorowicz <jonathan.gregorowicz@gmail.com>
2017-03-15 23:16:02 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Negative Splitter</h2>
+        </div>
+        <main>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App; 
